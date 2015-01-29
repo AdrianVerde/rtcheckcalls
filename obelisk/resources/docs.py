13367a9b9019ee2e6edca3225e5ae8682c5cb7b2
Pablo Martin (wdp) <caedes@sindominio.net>
2015-01-29 14:32:28 +0100

    Add user variables to doc template.

--- obelisk/resources/docs.py
+++ obelisk/resources/docs.py
@@ -3,6 +3,10 @@ from twisted.web.resource import Resource
 
 from obelisk.templates import print_template
 
+from obelisk import session
+from obelisk.model import Model
+from obelisk.asterisk.model import SipPeer
+
 class DocsResource(Resource):
     def render_GET(self, request):
 	parts = request.path.split("/")
@@ -14,6 +18,12 @@ class DocsResource(Resource):
 			content = print_template('docs/' + section, {})
 		except:
 			content = "la seccion no existe"
+	        user = session.get_user(request)
+	        if user:
+	            model = Model()
+	            peer = model.query(SipPeer).filter_by(regexten=user.voip_id).first()
+	            return print_template("content-pbx-lorea", {'content': content, 'user': user.voip_id, 'username': peer.name})
+
 		return print_template("content-pbx-lorea", {'content': content})
     def getChild(self, name, request):
         return self
